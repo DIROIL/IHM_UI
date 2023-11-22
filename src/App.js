51288ac13337a8c13642e62c5af4591e4890229a
Hilary <keubouhilary@gmail.com>
2023-11-22 03:21:34 +0100

    Modifier App.css et App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Menu from './Menu';
 
-function App() {
+const stylePage = {
+  backgroundColor: '#E0F8FF', // Couleur de fond gris
+  minHeight: '100vh', // Ajustez la hauteur de l'élément pour occuper au moins la hauteur de la vue
+  padding: '20px' // Ajoutez un espacement intérieur si nécessaire
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {stylePage} >
+      <Header/>
+      <Menu />      
     </div>
   );
 }
